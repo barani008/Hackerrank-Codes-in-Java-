00feaa1b30b5cce26cb76a3718a6e20ae5534992
Gangabarani <barani008@gmail.com>
2017-02-03 23:23:22 -0500

    Update peakElement.java

--- peakElement.java
+++ peakElement.java
@@ -1,3 +1,4 @@
+Key point here is that, at each mid, if you move towards the direction of semi-peak (5,7,x - > move right), you'll end up at some peak.
 public int findPeakElement(int[] a) {
         int low = 0, mid = 0, high = a.length - 1;
         while(low < high) {
